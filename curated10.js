65a2019d33d4e9fc0df476f3800f7a133c14d6db
swrdfgd <32948834+swrdfgd@users.noreply.github.com>
2025-07-19 18:04:08 +0800

    Add files via upload

--- curated10.js
+++ curated10.js
@@ -22,4 +22,40 @@ daftarCurated10 = [
 "endless.horse",
 "papertoilet.com",
 ];
+
+const dafontLetter = [
+  ['a', 261],
+  ['b', 373],
+  ['c', 287],
+  ['d', 182],
+  ['e', 94],
+  ['f', 149],
+  ['g', 162],
+  ['h', 210],
+  ['i', 47],
+  ['j', 76],
+  ['k', 118],
+  ['l', 145],
+  ['m', 298],
+  ['n', 90],
+  ['o', 66],
+  ['p', 165],
+  ['q', 34],
+  ['r', 191],
+  ['s', 421],
+  ['t', 177],
+  ['u', 27],
+  ['v', 70],
+  ['w', 105],
+  ['x', 9],
+  ['y', 23],
+  ['z', 27],
+  ['%23', 15]
+];
+
+let chosenDafont = dafontLetter[Math.floor(Math.random()*dafontLetter.length)];
+angkaDafont = chosenDafont[1]
+while (Math.random()<1/2){angkaDafont++}
+daftarCurated10.push('https://www.dafont.com/alpha.php?lettre='+chosenDafont[0]+'&page='+(1+Math.floor(Math.random()*angkaDafont)));
+
 daftarSitus = daftarSitus.concat(daftarCurated10);
