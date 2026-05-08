81a91f4b3acb56778d599360cc99d264b64018a6
swrdfgd <32948834+swrdfgd@users.noreply.github.com>
2026-05-08 09:32:07 +0800

    Add files via upload

--- curated3.js
+++ curated3.js
@@ -4,6 +4,12 @@ daftarCurated3 = [
 "proxx.app",
 "neal.fun",
 ["https://neal.fun/absurd-trolley-problems/", "Absurd Trolley Problems"],
+["https://neal-fun.firebaseapp.com/draw-your-island/", "Draw Your Island"],
+["https://web.archive.org/web/20210304063244/https://neal.fun/macaroni-draw/", "Macaroni Draw"],
+["https://web.archive.org/web/20220426063713/https://neal.fun/conquer-the-world/", "Conquer the World"],
+["https://web.archive.org/web/20180128230747/neal.fun/powerball", "Win The Powerball"],
+["https://neal-fun.firebaseapp.com/WIKI-FILES", "Wiki Files"],
+["https://web.archive.org/web/20191031144301/https://neal.fun/grandpas-art-show/", "Grandpa's Art Show"],
 
 ];
 daftarSitus = daftarSitus.concat(daftarCurated3);
